73ab8ebeb714649ba729653894aee2f24f6e3f9f
Anoof Shaikh <anoofs@gmail.com>
2017-11-22 06:41:18 +0530

    basic chatbot for onConnect and onDisconnect

--- app.js
+++ app.js
@@ -10,19 +10,48 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 })
 
+var bot = new Bot();
+function Bot(){
+	var that = this;
+	this.timeout = setTimeout(function(){
+		that.join();
+	}, 5000);
+}
+Bot.prototype.cancel = function(){
+	clearTimeout(this.timeout);
+};
+Bot.prototype.join = function(){
+	io.sockets.emit('new user', 'networkSupportBot');
+
+	// socket.on('user joined', this.user_joined_listener);
+	// socket.on('user left', this.user_left_listener);
+	// socket.on('new message', this.new_message_listener);
+	// socket.on('client left', this.client_left_listener);
+};
+Bot.prototype.leave = function(){
+	// io.sockets.disconnect();
+};
+Bot.prototype.userJoined = function(username){
+	io.sockets.emit('new message', {msg: "Hi there " + username + ", I provide support with networking", user: 'networkSupportBot'});
+};
+Bot.prototype.userLeft = function(username){
+	io.sockets.emit('new message', {msg: "Goodbye " + username + ", glad to be of help", user: 'networkSupportBot'});
+};
+
 io.sockets.on('connection', function(socket){
 	socket.on('new user', function(data, callback){
 		if(usernames.indexOf(data) != -1){
-			callback(false);		
+			callback(false);
 		} else {
 			callback(true);
 			socket.username = data;
 			usernames.push(socket.username);
-			updateUsernames(); 
+			updateUsernames();
+			bot.userJoined(socket.username);
 		}
 	});
 
-	//Update usernames	
+	//Update usernames
 	function updateUsernames(){
 		io.sockets.emit('usernames', usernames);
 	}
@@ -37,6 +66,17 @@ io.sockets.on('connection', function(socket){
 		if(!socket.username) return;
 		usernames.splice(usernames.indexOf(socket.username), 1);
 		updateUsernames()
-		
+		bot.userLeft(socket.username);
 	})
-})
+
+
+		// Bot.prototype.new_message_listener = function(data){
+		//   var socket = this.socket;
+		//   if(data.message=='Hello, HAL. Do you read me, HAL?')
+		//     socket.emit('message', 'Affirmative, '+data.username+'. I read you.');
+		// };
+		// Bot.prototype.client_left_listener = function(data){
+		//   this.leave();
+		// };
+	   /********************** CREATING BOT, AFTER 5 SECONDS HE WILL JOIN - SEE CONSTRUCTOR OF Bot *********/
+	});
